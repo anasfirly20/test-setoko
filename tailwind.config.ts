15c8e14d403674f4d6dbeb57c2afc1ad2a512f20
Anas Firly <anasfirly20@gmail.com>
2023-11-08 14:30:59 +0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,67 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+// Next ui
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      // colors: {
+      //   spacecadet: "#1A334F",
+      //   gunmetal: "#182431",
+      //   dark: "#212121",
+      //   crayola: "#A7ADBB",
+      //   antiflash: "#EFF1F4",
+      //   lightgray: "#C7C7C7",
+      // },
+      padding: {
+        shorter4: "1vw",
+        shorter3: "2vw",
+        shorter2: "3vw",
+        shorter: "4vw",
+        normal: "6vw",
+        longer: "8vw",
+        longer2: "10vw",
+        longer3: "12vw",
+        longer4: "14vw",
+        longer5: "16vw",
+        longer6: "18vw",
+        longer7: "20vw",
+        longer8: "22vw",
+        longer9: "24vw",
+        longer10: "26vw",
+      },
+      margin: {
+        shorter4: "1vw",
+        shorter3: "2vw",
+        shorter2: "3vw",
+        shorter: "4vw",
+        normal: "6vw",
+        longer: "8vw",
+        longer2: "10vw",
+        longer3: "12vw",
+        longer4: "14vw",
+        longer5: "16vw",
+        longer6: "18vw",
+        longer7: "20vw",
+        longer8: "22vw",
+        longer9: "24vw",
+        longer10: "26vw",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class",
+  plugins: [nextui()],
+};
+export default config;
