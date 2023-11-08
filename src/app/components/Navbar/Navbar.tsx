03934285edd239393bd57205b4db69dc67d2bac7
Anas Firly <anasfirly20@gmail.com>
2023-11-08 14:41:57 +0700

    feat: adding navbar and adjusting font to Poppins

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+  return (
+    <nav className='py-6 px-[10vw] flex justify-between items-center'>
+    <h1>Github Repositories Explorer</h1>
+    </nav>
+  )
+}
